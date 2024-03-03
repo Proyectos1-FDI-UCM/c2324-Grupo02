f2ec2322e21534903fe6f8230f3eb5bafc58a3f7
Lparres2000 <Lparres2000@gmail.com>
2024-03-03 12:52:59 +0100

    PauseInput

---
+++ TaladroJuego/Assets/Systems/Input/PauseInput.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace InputSystem
+{
+    internal class PauseInput : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference _pauseInputActionReference;
+        //[SerializeField] private PauseRequesterObject _pauseRequester;
+        [SerializeField] private GameObject _HUDPanel;
+        [SerializeField] private GameObject _settingsPanel;
+
+        private bool _isGamePaused = false;
+
+        private void Awake() {
+
+            _pauseInputActionReference.action.performed += OnPausedInputStarted;
+
+        }
+
+        private void OnPausedInputStarted(InputAction.CallbackContext obj) {
+            SwitchPause();
+        }
+
+        public void SwitchPause() {
+            _isGamePaused = !_isGamePaused;
+
+            _HUDPanel.SetActive(!_isGamePaused);
+            _settingsPanel.SetActive(_isGamePaused);  
+        }
+
+        #region ENABLE / DISABLE INPUTACTIONS
+
+        private void OnEnable() {
+            _pauseInputActionReference.action.Enable();
+        }
+
+        private void OnDisable() {
+            _pauseInputActionReference.action.Disable();
+        }
+
+        #endregion
+    }
+}
