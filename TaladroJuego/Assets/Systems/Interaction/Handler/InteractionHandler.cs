ff1f578950ee34581e26b22afebd4489dd896ce7
ChichoRDucm <josenaro@ucm.es>
2024-03-13 09:26:20 +0100

    Fixed missing type in InteractionSystem

--- TaladroJuego/Assets/Systems/Interaction/Handler/InteractionHandler.cs
+++ TaladroJuego/Assets/Systems/Interaction/Handler/InteractionHandler.cs
@@ -1,4 +1,3 @@
-using InteractionSystem.Adapter;
 using InteractionSystem.Locator;
 using UnityEngine;
 
@@ -6,18 +5,16 @@ namespace InteractionSystem.Handler
 {
     internal class InteractionHandler : MonoBehaviour
     {
-        private IInteractorAdapter _interactorAdapter;
         private IInteractableLocator _interactableLocator;
 
         private void Awake()
         {
-            _interactorAdapter = GetComponentInChildren<IInteractorAdapter>();
             _interactableLocator = GetComponentInChildren<IInteractableLocator>();
         }
 
         public bool Interact()
         {
-            return _interactorAdapter.Interact(_interactableLocator);
+            return true;
         }
     }
 }
---
+++ TaladroJuego/Assets/Systems/Interaction/Handler/InteractorHandler.cs
@@ -0,0 +1,24 @@
+﻿using InteractionSystem.Interactable;
+using InteractionSystem.Interactor;
+using InteractionSystem.Locator;
+
+namespace InteractionSystem.Handler
+{
+    internal readonly struct InteractorHandler : IInteractorHandler
+    {
+        private readonly IInteractableLocator _interactableLocator;
+
+        public InteractorHandler(IInteractableLocator interactableLocator)
+        {
+            _interactableLocator = interactableLocator;
+        }
+
+        public bool Handle<T>(IInteractor<T> interactor)
+        {
+            bool success = true;
+            foreach (IInteractable<T> interactable in _interactableLocator.GetInteractables<T>())
+                success &= interactable.Accept(interactor);
+            return success;
+        }
+    }
+}
