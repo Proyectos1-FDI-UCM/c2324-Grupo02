522361c4bae3a989c535fb3309f838c84bd2fb01
ChichoRDucm <josenaro@ucm.es>
2024-03-13 10:37:58 +0100

    Added InteractableLocators

--- TaladroJuego/Assets/Systems/Interaction/Interactable/IInteractable.cs
+++ TaladroJuego/Assets/Systems/Interaction/Interactable/IInteractable.cs
@@ -2,10 +2,20 @@ using InteractionSystem.Interactor;
 
 namespace InteractionSystem.Interactable
 {
-    public interface IInteractable<out T>
+    public interface IInteractable
     {
+        bool Accept<T>(IInteractor<T> interactor);
+    }
+
+    public interface IInteractable<out T> : IInteractable
+    {
+        bool IInteractable.Accept<U>(IInteractor<U> interactor) =>
+            interactor is IInteractor<T> tInteractor
+            && Accept(tInteractor);
+
         bool Accept<TInteractor>(TInteractor interactor) where TInteractor : IInteractor<T>;
     }
+
     //public interface IInteractable<in TInteractor, out T> where TInteractor : IInteractor<T>
     //{
     //    bool Accept(TInteractor interactor);
---
+++ TaladroJuego/Assets/Systems/Interaction/Locator/TriggerInteractableLocator.cs
@@ -0,0 +1,50 @@
+﻿using InteractionSystem.Interactable;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace InteractionSystem.Locator
+{
+    internal class TriggerInteractableLocator : MonoBehaviour, IInteractableLocator
+    {
+        [SerializeField]
+        private LayerMask _layerMask;
+
+        [field: SerializeField]
+        public UnityEvent<IInteractable> InteractableAdded { get; private set; }
+        [field: SerializeField]
+        public UnityEvent<IInteractable> InteractableRemoved { get; private set; }
+
+        private List<IInteractable> _foundObjects;
+        private InteractableLocator _interactableLocator;
+
+        private void Awake()
+        {
+            _foundObjects = new List<IInteractable>();
+            _interactableLocator = new InteractableLocator(_foundObjects);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (InteractableLocatorExtensions.InLayerMask(collision.gameObject, _layerMask)
+                && InteractableLocatorExtensions.TryGetComponentInChildren(collision.gameObject, out IInteractable interactable))
+            {
+                _foundObjects.Add(interactable);
+                InteractableAdded.Invoke(interactable);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (InteractableLocatorExtensions.InLayerMask(collision.gameObject, _layerMask)
+                && InteractableLocatorExtensions.TryGetComponentInChildren(collision.gameObject, out IInteractable interactable))
+            {
+                _foundObjects.Remove(interactable);
+                InteractableRemoved.Invoke(interactable);
+            }
+        }
+
+        public IEnumerable<IInteractable<T>> GetInteractables<T>() =>
+            _interactableLocator.GetInteractables<T>();
+    }
+}
