329f8a48d5c11e1a0f7d3108f280faafee0e75ca
TelmoK <telmo.kiss@gmail.com>
2024-03-15 12:18:39 +0100

    Null reference exception fixed in interactor

--- TaladroJuego/Assets/Systems/Interaction-Implementations/Bomb Interaction/Interactor/ExplosiveInteractor.cs
+++ TaladroJuego/Assets/Systems/Interaction-Implementations/Bomb Interaction/Interactor/ExplosiveInteractor.cs
@@ -1,16 +1,27 @@
 using InteractionImplementationsSystem.BombInteraction.Interactable;
+using InteractionSystem.Handler;
 using InteractionSystem.Interactor;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace InteractionImplementationsSystem.BombInteraction.Interactor
 {
-    internal class ExplosiveInteractor : MonoBehaviour, IInteractor<Explosive>
+    internal class ExplosiveInteractor : MonoBehaviour, IInteractor, IInteractor<Explosive>
     {
+        private Interactor<Explosive> _interactor;
+
+        private void Awake()
+        {
+            _interactor = new Interactor<Explosive>(this);
+        }
+
         public bool InteractWith(Explosive explosive)
         {
             return explosive.Explode();
+        }
+
+        public bool Accept(IInteractorHandler handler)
+        {
+            return _interactor.Accept(handler);
         }
     }
 }
