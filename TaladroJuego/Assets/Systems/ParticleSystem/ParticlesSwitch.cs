6ad0c062c96daeb22f95b264038c93848a3b9550
Lparres2000 <Lparres2000@gmail.com>
2024-03-19 21:18:36 +0100

    Particle system conectado con terrain

--- TaladroJuego/Assets/Systems/ParticleSystem/ParticlesSwitch.cs
+++ TaladroJuego/Assets/Systems/ParticleSystem/ParticlesSwitch.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+using TerrainSystem.Requestable.Retriever.Observable;
+using System.Collections.Generic;
+using UpgradesSystem.Flyweight;
+
 namespace Particles
 {
     [RequireComponent(typeof(ParticleSystem))]
@@ -7,9 +11,25 @@ namespace Particles
     {
         private ParticleSystem ps;
 
+        [SerializeField] private IObservableTerrainData<TerrainModification> terrainData;
+
+        [SerializeField] private Dictionary<ResourceType, uint> terrainType;
 
         private void Awake() {
             ps = GetComponent<ParticleSystem>();
+
+            terrainData = GetComponent<IObservableTerrainData<TerrainModification>>();
+
+            terrainData.DataRetrieved += TerrainData;
+
+            terrainType.Add(ResourceType.Lead, 1);
+        }
+
+        private void TerrainData(object sender, TerrainModification e) {
+            print(e.amount);
+            print(e.terrainType);
+            if (e.amount > 0) ps.Play();
+            else ps.Stop();
         }
     }
 }
