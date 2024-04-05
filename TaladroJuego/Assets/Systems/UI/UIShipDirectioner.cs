9931a7e4d64891de1e57b97800d50e45297bf81d
Lparres2000 <Lparres2000@gmail.com>
2024-04-05 13:03:14 +0200

    Intento de coger una posicion de la ui (no funciona jiji))

--- TaladroJuego/Assets/Systems/UI/UIShipDirectioner.cs
+++ TaladroJuego/Assets/Systems/UI/UIShipDirectioner.cs
@@ -29,22 +29,23 @@ namespace UISystem
         public void OnPointerDown(PointerEventData eventData)
         {
             directionerDragged = true;
-            print("pointerdown");
+            //print("pointerdown");
         }
         public void OnPointerUp(PointerEventData eventData)
         {
             directionerDragged = false;
-            print("pointerup");
+            //print("pointerup");
         }
 
         public void OnDrag(PointerEventData eventData)
         {
+            //Vector3 UIElementPosition = new Vector3((transform.position.x + 10) / (10 * 2), (transform.position.y + 10 / (640 / 360)) / (10 / (640 / 360) * 2), 0.0f);
             transform.up = (Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue()) - new Vector3(0.87f, 0.18f, 0.0f)).normalized;
-            print("ondrag");
         }
 
         private void FixedUpdate()
         {
+            
             if (shipRotationMovementFacade != null)
             {
                 if (directionerDragged)
