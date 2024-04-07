95ae463eedcdf73309687449aabb828ac5dd1484
DiegoCaballeroRuiz <dicaball@ucm.es>
2024-04-07 20:35:19 +0200

    Feedback visual de daño y cura añadido :+1:

--- TaladroJuego/Assets/Systems/Animation/Status Update Animnation/OnStatusUpdateAnimator.cs
+++ TaladroJuego/Assets/Systems/Animation/Status Update Animnation/OnStatusUpdateAnimator.cs
@@ -3,38 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using StatusSystem;
 
-internal class OnStatusUpdateAnimator : MonoBehaviour
+namespace AnimationSystem.Status
 {
-    [SerializeField] private StatusParameter _statusParameter;
-    [SerializeField] private SpriteRenderer _spriteRenderer;
-
-    [SerializeField] private Color _hitColor;
-    [SerializeField] private Color _healColor;
-
-    private float _lastStatusValue;
-    private Color _baseSpriteColor;
-
-    private IEnumerator PlayAnimation(Color transitionColor)
+    internal class OnStatusUpdateAnimator : MonoBehaviour
     {
-        _spriteRenderer.color = Color.Lerp(_baseSpriteColor, transitionColor, 0.8f * Time.fixedDeltaTime);
-        yield return new WaitForSeconds(0.2f);
-        _spriteRenderer.color = Color.Lerp(transitionColor, _baseSpriteColor, 0.8f * Time.fixedDeltaTime);
-        yield return new WaitForSeconds(0.2f);
+        [SerializeField] private StatusParameter _statusParameter;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+
+        [SerializeField] private Color _hitColor;
+        [SerializeField] private Color _healColor;
+
+        private float _lastStatusValue;
+        private Color _baseSpriteColor;
+
+        void Start()
+        {
+            _lastStatusValue = _statusParameter.Value;
+            _baseSpriteColor = _spriteRenderer.color;
+        }
+
+        private IEnumerator PlayAnimation(Color transitionColor)
+        {
+            _spriteRenderer.color = Color.Lerp(_baseSpriteColor, transitionColor, 1);
+            yield return new WaitForSeconds(0.2f);
+            _spriteRenderer.color = Color.Lerp(transitionColor, _baseSpriteColor, 1);
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        public void HandleStatusChange()
+        {
+            Color color;
+            float value = _statusParameter.Value;
+
+            if (_lastStatusValue > value) color = _hitColor;
+            else color = _healColor;
+
+            _lastStatusValue = value;
+
+            StartCoroutine(PlayAnimation(color));
+        }
     }
 
-    public void HandleStatusChange()
-    {
-        StartCoroutine(PlayAnimation(_hitColor));
-    }
-
-    void Start()
-    {
-        _lastStatusValue = _statusParameter.Value;
-        _baseSpriteColor = _spriteRenderer.color;
-    }
-
-    void Update()
-    {
-        
-    }
 }
