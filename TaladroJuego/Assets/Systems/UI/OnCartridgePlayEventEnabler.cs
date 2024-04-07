fa292e882db653892fef8ff5f9e2a2ba1a96ea81
ChichoRDucm <josenaro@ucm.es>
2024-04-07 07:06:21 +0200

    Merge branch 'ShaderMaster'

---
+++ TaladroJuego/Assets/Systems/UI/OnCartridgePlayEventEnabler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ContextualDialogueSystem;
+using ContextualDialogueSystem.Event;
+using InteractionImplementationsSystem.CartridgeInteraction.Interactable;
+
+namespace UISystem
+{
+    internal class OnCartridgePlayEventEnabler : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
+
