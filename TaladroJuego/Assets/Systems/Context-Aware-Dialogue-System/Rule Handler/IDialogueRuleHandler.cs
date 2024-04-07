cde1a103c7291989fd4e6f9bfadf79c2ead16ab2
ChichoRDucm <josenaro@ucm.es>
2024-04-07 23:59:09 +0200

    Update IDialogueRuleHandler.cs

--- TaladroJuego/Assets/Systems/Context-Aware-Dialogue-System/Rule Handler/IDialogueRuleHandler.cs
+++ TaladroJuego/Assets/Systems/Context-Aware-Dialogue-System/Rule Handler/IDialogueRuleHandler.cs
@@ -5,13 +5,22 @@ using System.Threading.Tasks;
 
 namespace ContextualDialogueSystem.RuleHandler
 {
-    public interface IDialogueRuleHandler<in TRuleContent> : ISubscribableDialogueRuleHandler
+
+    public interface IDialogueRuleHandler
+    {
+        Task<bool> HandleRule<TRuleContent>(IDialogueRule<TRuleContent, ICriteria> dialogueRule);
+    }
+
+    public interface IDialogueRuleHandler<in TRuleContent> : ISubscribableDialogueRuleHandler, IDialogueRuleHandler
     {
         bool ISubscribableDialogueRuleHandler.TrySubscribeToDialogueEvent(IObservableDialogueEvent dialogueEvent) =>
             dialogueEvent != null && dialogueEvent.Subscribe(this);
 
         bool ISubscribableDialogueRuleHandler.TryUnsubscribeFromDialogueEvent(IObservableDialogueEvent dialogueEvent) =>
             dialogueEvent != null && dialogueEvent.Unsubscribe(this);
+
+        Task<bool> IDialogueRuleHandler.HandleRule<URuleContent>(IDialogueRule<URuleContent, ICriteria> dialogueRule) =>
+            dialogueRule is IDialogueRule<TRuleContent, ICriteria> rule ? HandleRule(rule) : Task.FromResult(false);
 
         Task<bool> HandleRule(IDialogueRule<TRuleContent, ICriteria> dialogueRule);
     }
