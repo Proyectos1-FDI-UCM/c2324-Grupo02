b8482c59de8482a0b5e584b15649d692e5738e9a
imVictorVM <vvalen01@ucm.es>
2024-04-13 12:25:45 +0200

    pequeño cambio en onFuelEmpty

--- TaladroJuego/Assets/Systems/UI/OnFuelEmpty.cs
+++ TaladroJuego/Assets/Systems/UI/OnFuelEmpty.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 using ResourceCollectionSystem;
 using UpgradesSystem.Flyweight;
 using Codice.Client.BaseCommands.WkStatus.Printers;
+using MovementSystem.LastFart;
 
 namespace UISystem
 {
     public class OnFuelEmpty : MonoBehaviour
     {
+
+        private LastFartLauncher  _lastFartLauncher;
+
         [SerializeField] private ResourcesContainer resources;
         [SerializeField] private GameObject gameOverPanel;
         public void NoFuel()
         {
             resources.ResourceQuantities.TryGetValue(ResourceType.Charcoal, out int amount);
-            if (amount < 9) // No se acceder al coste de la mejora
+            if (amount < 9 && !_lastFartLauncher.IsFartEnabled) // No se acceder al coste de la mejora
             {
                 gameOverPanel.SetActive(true);
             }
