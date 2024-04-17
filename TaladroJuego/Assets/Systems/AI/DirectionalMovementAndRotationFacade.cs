e29919d303f8b6b3637bc445a6c0e8ffbe510622
ChichoRDucm <josenaro@ucm.es>
2024-04-18 01:10:07 +0200

    yveL

---
+++ TaladroJuego/Assets/Systems/AI/DirectionalMovementAndRotationFacade.cs
@@ -0,0 +1,28 @@
+﻿using MovementSystem.Facade;
+using UnityEngine;
+
+namespace AISystem
+{
+    internal class DirectionalMovementAndRotationFacade : MonoBehaviour, IMovementFacade<Vector2>
+    {
+        [SerializeField]
+        private GameObject _movementFacadeObject;
+        [SerializeField]
+        private GameObject _rotationFacadeObject;
+
+        private IMovementFacade<Vector2> _movementFacade;
+        private IMovementFacade<Vector2> _rotationFacade;
+
+        private void Awake()
+        {
+            _movementFacade = _movementFacadeObject.GetComponent<IMovementFacade<Vector2>>();
+            _rotationFacade = _rotationFacadeObject.GetComponent<IMovementFacade<Vector2>>();
+        }
+
+        public void Move(Vector2 input)
+        {
+            _movementFacade.Move(input);
+            _rotationFacade.Move(input);
+        }
+    }
+}
--- TaladroJuego/Assets/Systems/AI/SinusoidalMovementDirectionRotatorApplier.cs
+++ TaladroJuego/Assets/Systems/AI/SinusoidalMovementDirectionRotatorApplier.cs
@@ -1,34 +1,44 @@
-using MovementSystem.Facade;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace AISystem
 {
     internal class SinusoidalMovementDirectionRotatorApplier : MonoBehaviour
     {
-        [SerializeField] private Transform[] _bones;
-        [SerializeField] private Rigidbody2D _rigidbody;
-        [SerializeField] private AnimationCurve _curve;
-        private (IMovementFacade<Vector2> movementFacade, Transform transform)[] _movementFacades;
+        [SerializeField]
+        [Min(0)]
+        private int _segmentCount;
+
+        [SerializeField]
+        private LineRenderer _lineRenderer;
+        [SerializeField]
+        private Transform _target;
+
+        [SerializeField]
+        private float _segmentSeparation;
+        [SerializeField]
+        private float _smoothedSpeed;
+
+        private Vector3[] _segmentPositions;
+        private Vector3[] _segementVelocities;
 
         private void Awake()
         {
-            _movementFacades = _bones.Select(bone => (bone.GetComponent<IMovementFacade<Vector2>>(), bone)).ToArray();
+            _lineRenderer.positionCount = _segmentCount;
+            _segmentPositions = new Vector3[_segmentCount];
+            _segementVelocities = new Vector3[_segmentCount];
         }
 
         private void Update()
         {
-            Transform previousTransform = _movementFacades[0].transform;
-            for(int i = 0; i < _movementFacades.Length; i++)
+            _segmentPositions[0] = _target.position;
+
+            for (int i = 1; i < _segmentPositions.Length; i++)
             {
-                float t = 1 - (i / _movementFacades.Length);
-                (IMovementFacade<Vector2> mF, Transform tF) = _movementFacades[i];
-                Vector3 boneDirection = Vector3.Slerp(tF.right, previousTransform.InverseTransformDirection(_rigidbody.velocity.normalized), _curve.Evaluate(t + Mathf.Sin(1 - t) * 2 * Mathf.PI));
-                mF.Move(boneDirection);  
-                previousTransform = tF;
+                Vector3 targetPosition = _segmentPositions[i - 1] + (_segmentPositions[i] - _segmentPositions[i - 1]).normalized * _segmentSeparation;
+                _segmentPositions[i] = Vector3.SmoothDamp(_segmentPositions[i], targetPosition, ref _segementVelocities[i], _smoothedSpeed);
             }
+
+            _lineRenderer.SetPositions(_segmentPositions);
         }
     }
 
