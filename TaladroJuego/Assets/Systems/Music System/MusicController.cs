24202baa63b714bb1e886b6b594522b63c3afd85
imVictorVM <vvalen01@ucm.es>
2024-04-17 21:05:04 +0200

    levi theme lets goo

--- TaladroJuego/Assets/Systems/Music System/MusicController.cs
+++ TaladroJuego/Assets/Systems/Music System/MusicController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicController : MonoBehaviour
+namespace MusicSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    internal class MusicController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private AudioSource _songFase1; //Cancion Fase 1
+        [SerializeField] private AudioSource _songFase2; //Cancion Fase 2
+        [SerializeField] private AudioSource _songFase3; //Cancion Fase 3
+        [SerializeField] private AudioSource _songFase4; //Cancion Fase 4
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision != null)
+            {
+                _songFase2.volume = 1;
+            }
+        }
     }
 }
