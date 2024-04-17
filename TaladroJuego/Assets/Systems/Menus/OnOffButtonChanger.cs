02228fa571c15bb4635f440fda5d250c6f741e4b
imVictorVM <vvalen01@ucm.es>
2024-04-17 17:13:17 +0200

    el boton onoff ahora onoffea

---
+++ TaladroJuego/Assets/Systems/Menus/OnOffButtonChanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MenusSystem
+{
+    internal class OnOffButtonChanger : MonoBehaviour
+    {
+        [SerializeField] private GameObject _textOn;
+        [SerializeField] private GameObject _textOff;
+
+        private bool _change = false;
+
+        public void OnButtonPressed()
+        {
+            _textOn.gameObject.SetActive(!_change);
+            _textOff.gameObject.SetActive(_change);
+            _change = !_change;
+        }
+
+
+    }
+}
