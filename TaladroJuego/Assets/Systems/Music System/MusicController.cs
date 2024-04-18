c85435b01f2294b557f17f34fa22924520a1471a
imVictorVM <vvalen01@ucm.es>
2024-04-18 18:19:13 +0200

    prefabs de musica y zonas implementados
    triggers que delimitan cada zona

--- TaladroJuego/Assets/Systems/Music System/MusicController.cs
+++ TaladroJuego/Assets/Systems/Music System/MusicController.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 namespace MusicSystem
 {
     internal class MusicController : MonoBehaviour
     {
-        [SerializeField] private AudioSource _songFase1; //Cancion Fase 1
-        [SerializeField] private AudioSource _songFase2; //Cancion Fase 2
-        [SerializeField] private AudioSource _songFase3; //Cancion Fase 3
-        [SerializeField] private AudioSource _songFase4; //Cancion Fase 4
-
+        [SerializeField] private AudioSource _song; 
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision != null)
             {
-                _songFase2.volume = 1;
+                ChangeVolume();
             }
+        }
+
+        private void ChangeVolume()
+        {
+            _song.volume = 1;
         }
     }
 }
